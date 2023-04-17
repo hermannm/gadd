4470b907d1a43df33ae3fdc8b6fb8e2e027ce910
hermannm <hermann.morkrid@gmail.com>
2023-04-18 01:20:38 +0200

    move logic out from main function to submodules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,95 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, Stdout, Write};
+
+use anyhow::{Context, Result};
+use crossterm::{
+    cursor,
+    terminal::{self, ClearType},
+    QueueableCommand,
+};
+use git2::Repository;
+use input::{render_input_controls, user_input_event_loop};
+
+use crate::change_list::ChangeList;
+
+mod change_list;
+mod input;
+mod utils;
+
+fn main() -> Result<()> {
+    let _cleanup = Cleanup;
+
+    let mut stdout = stdout();
+
+    terminal::enable_raw_mode()?;
+
+    stdout
+        .queue(terminal::EnterAlternateScreen)?
+        .queue(cursor::Hide)?;
+
+    let repository = Repository::discover(".").context("Failed to open repository")?;
+
+    let statuses = repository
+        .statuses(None)
+        .context("Failed to get statuses of changes in repository")?;
+
+    let mut change_list = ChangeList::new(&repository, &statuses)?;
+
+    render(&mut stdout, &change_list)?;
+
+    user_input_event_loop(&mut stdout, &mut change_list)?;
+
+    Ok(())
+}
+
+pub(self) fn render(stdout: &mut Stdout, change_list: &ChangeList) -> Result<()> {
+    stdout.queue(terminal::Clear(ClearType::All))?;
+
+    change_list.render(stdout)?;
+
+    render_input_controls(stdout)?;
+
+    stdout.write_all("\r".as_bytes())?;
+
+    stdout.flush()?;
+
+    Ok(())
+}
+
+struct Cleanup;
+
+impl Drop for Cleanup {
+    fn drop(&mut self) {
+        let mut stdout = stdout();
+
+        let alternate_screen_err = stdout
+            .queue(terminal::LeaveAlternateScreen)
+            .context("Failed to leave the alternate screen")
+            .err();
+
+        let show_cursor_err = stdout
+            .queue(cursor::Show)
+            .context("Failed to re-enable the cursor")
+            .err();
+
+        let flush_err = stdout
+            .flush()
+            .context("Failed to flush terminal cleanup")
+            .err();
+
+        let raw_mode_err = terminal::disable_raw_mode()
+            .context("Failed to disable terminal raw mode")
+            .err();
+
+        for error in [
+            alternate_screen_err,
+            show_cursor_err,
+            flush_err,
+            raw_mode_err,
+        ]
+        .into_iter()
+        .flatten()
+        {
+            println!("Error on cleanup: {error}");
+        }
+    }
 }
