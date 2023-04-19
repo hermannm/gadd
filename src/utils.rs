0952d98b329f1d306440b0b162f52eb5bf501b3c
hermannm <hermann.morkrid@gmail.com>
2023-04-20 01:42:12 +0200

    fix import errors when cross-compiling

--- src/utils.rs
+++ src/utils.rs
@@ -1,19 +1,18 @@
-use std::{ffi::OsStr, path::Path};
+use std::path::Path;
 
 use git2::{IndexEntry, IndexTime, Oid};
 
 /// From git2 crate: https://docs.rs/git2/0.17.1/src/git2/util.rs.html#86
 #[cfg(unix)]
 pub(super) fn bytes_to_path(bytes: &[u8]) -> &Path {
-    use std::os::unix::prelude::*;
+    use std::{ffi::OsStr, os::unix::prelude::*};
     Path::new(OsStr::from_bytes(bytes))
 }
 
 /// From git2 crate: https://docs.rs/git2/0.17.1/src/git2/util.rs.html#91
 #[cfg(windows)]
 pub(super) fn bytes_to_path(bytes: &[u8]) -> &Path {
-    use std::str;
-    Path::new(str::from_utf8(bytes).unwrap())
+    Path::new(std::str::from_utf8(bytes).unwrap())
 }
 
 pub(super) fn new_index_entry(id: Oid, mode: u32, path: Vec<u8>) -> IndexEntry {
